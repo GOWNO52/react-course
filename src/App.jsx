d3462c2dc22993716fe4849ae6116580f36f8829
Riza <rizabors0@gmail.com>
2025-07-08 19:00:59 -0600

    homework-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import TaskTwo from './components/TaskTwo'
-import TascThree from './components/TascThree'
+
 
 
 function App() {
   return (
     <>
-      <TaskTwo />
-      <TascThree></TascThree>
+      
     </>
   )
 }
